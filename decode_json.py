3e15d1e189fee578493742275f4ecea4ac8a8d68
Kahlail <k40075@outlook.com>
2024-03-13 15:52:25 +0000

    write unit tests and refactor flatten_json_object, ensure flatten_json_object can pass tests, correct typos in comments etc

--- decode_json.py
+++ decode_json.py
@@ -30,13 +30,13 @@ class json_decoding_tool:
     # Converts a json object to CSV and outputs the file to the root (data_tools) directory.
     def decode_json_to_csv(self, output_csv_filename: str) -> str:
         i = json.loads(self.json_object)
-        df = pd.json_normalize(i)
+        df = pd.read_json(i)
         df.to_csv(f'{output_csv_filename}.csv', index=False)
         return print(f'json object successsfully converted to csv as {output_csv_filename}.csv')
     
     # Converts a json object to Excel and outputs the file to the root (data_tools) directory.
     def decode_json_to_excel(self, output_excel_filename: str) -> str:
         i = json.loads(self.json_object)
-        df = pd.json_normalize(i)
+        df = pd.read_json(i)
         df.to_excel(f'{output_excel_filename}.xlsx', index=False)
         return print(f'json object successsfully converted to excel sheet as {output_excel_filename}.xlsx')
--- tests/test_flatten_json.py
+++ tests/test_flatten_json.py
@@ -1,12 +1,60 @@
-import json
-# from GitHub_Profile.data_tools.flatten_json_object import flatten_json_object
+import pytest
 from data_tools.flatten_json_object import flatten_json_object
 
-# Arrange
-test_nested_json = {
+# This test checks that the function works with the expected json input
+def test_will_flatten_nested_json():
+    #Arrange
+    nested_json = {
+  "person": {
+    "name": "John Doe",
+    "age": 30,
+    "address": {
+      "street": "123 Main St",
+      "city": "Anytown",
+      "state": "Anystate",
+      "postal_code": "12345"
+    },
+  },
+  "company": {
+    "name": "Acme Corporation",
+    "industry": "Technology",
+    "employees": [
+      {
+        "name": "Jane Smith",
+        "position": "Software Engineer"
+      },
+      {
+        "name": "Bob Johnson",
+        "position": "Project Manager"
+      }
+    ]
+  }
 }
+    expected_result = {
+    "person_name": "John Doe",
+    "person_age": 30,
+    "person_address_street": "123 Main St",
+    "person_address_city": "Anytown",
+    "person_address_state": "Anystate",
+    "person_address_postal_code": "12345",
+    "company_name": "Acme Corporation",
+    "company_industry": "Technology",
+    "company_employees_0_name": "Jane Smith",
+    "company_employees_0_position": "Software Engineer",
+    "company_employees_1_name": "Bob Johnson",
+    "company_employees_1_position": "Project Manager"
+}
+    # Act
+    actual_result = flatten_json_object(nested_json)
+    
+    # Assert
+    assert actual_result == expected_result
 
-test_circular_reference_json = {
+
+# This test checks the function can handle circular references correctly
+def test_will_flatten_json_with_circular_references():
+    # Arrange
+    circular_reference_json = {
     "name": "Alice",
     "friends": [
         {
@@ -29,24 +77,61 @@ test_circular_reference_json = {
         }
     ]
 }
-
-test_empty_json = {}
-
-# This test checks to see if the function works with the expected json input
-def test_flatten_json_array_happy_path():
-    #Arrange
     expected_result = {
-    'name': 'Alice',
-    'friends_name': 'Bob',
-    'friends_friends_0_name': 'Charlie',
-    'friends_name': 'Charlie'
+        'name': 'Alice', 
+        'friends_0_name': 'Bob', 
+        'friends_1_name': 'Charlie', 
+        'friends_0_friends_0_name': 'Charlie', 
+        'friends_0_friends_0_friends_0_name': 'Bob'
 }
     # Act
-    actual_result = flatten_json_object(test_json_array)
-    
+    actual_result = flatten_json_object(circular_reference_json)
+
     # Assert
     assert actual_result == expected_result
 
 
-# def test_flatten_json_object_happy_path():
-#     test_data = json.dumps(test_json_array)
+# This test checks that the function can handle an empty json
+def test_can_handle_empty_input():
+    # Arrange
+    empty_json = {}
+    expected_result = "Error: You provided a null value."
+
+    # Act
+    actual_result = flatten_json_object(empty_json)
+
+    # Assert
+    assert actual_result == expected_result
+    
+# This test checks that the function can handle an unexpected numeric input
+def test_can_handle_unexpected_input_numeric():
+    # Arrange
+    not_json_is_numeric = 23940
+    expected_message = "This function expects a dict or list, you provided: <class 'int'>"
+
+    # Act
+    with pytest.raises(ValueError, match=expected_message):
+        flatten_json_object(not_json_is_numeric)
+    
+
+# This test checks that the function can handle an unexpected string input
+def test_can_handle_unexpected_input_string():
+    # Arrange
+    not_json_is_str = "Hello, world!"
+    expected_message = "This function expects a dict or list, you provided: <class 'str'>"
+
+    # Act
+    with pytest.raises(ValueError, match=expected_message):
+        flatten_json_object(not_json_is_str)
+    
+
+# This test checks that the function can handle an unexpected tuple input
+def test_can_handle_unexpected_input_tuple():
+    # Arrange
+    not_json_is_tuple = ("Hello", 3, "is my lucky number!")
+    expected_message = "This function expects a dict or list, you provided: <class 'tuple'>"
+    
+    # Act
+    with pytest.raises(ValueError, match=expected_message):
+        flatten_json_object(not_json_is_tuple)
+   
